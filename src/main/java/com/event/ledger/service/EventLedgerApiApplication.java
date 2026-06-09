28f64e47b28023faf750408a308e0fcaff165e7f
Hariharasudhan K <sudhan4u.hari@gmail.com>
2026-06-09 18:34:26 +0530

    Event Ledger API - Initial Commit

---
+++ src/main/java/com/event/ledger/service/EventLedgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.event.ledger.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLedgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLedgerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event/ledger/service/EventLedgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.ledger.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLedgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
